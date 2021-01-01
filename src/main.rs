9d8a3a72ad39616eaa929ddc1fbd6793d354d00c
etteryand0 <etteryand@gmail.com>
2021-01-01 18:48:06 +0900

    guessing function and loop break

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::io;
+use rand::Rng; // cargo doc --open
+use std::cmp::Ordering;
+
 fn main() {
     println!("Hello, world!");
+    
+    let secret_number = rand::thread_rng().gen_range(1,100);
+    println!("Secret number {}", secret_number);
+    
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line!");
+
+        let guess: u32 = guess.trim()
+                            .parse()
+                            .expect("Please type a number!");
+
+        println!("You guessed {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("{}You guessed it!{}", "\x1b[32m", "\x1b[0m");
+                break;
+            }
+        }
+    }
 }
